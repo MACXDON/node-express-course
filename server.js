fd5a0064f635af5edcab32283c4aa8b45b4564ba
MACXDON <72674976+MACXDON@users.noreply.github.com>
2021-10-07 23:34:33 -0500

    update to POST

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
